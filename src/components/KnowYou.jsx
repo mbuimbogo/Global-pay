673f07916fe9b51ae48b942ef74bf03c5b947ab7
mbuimbogo <mbuimbogo99@gmail.com>
2023-04-12 01:11:53 +0300

    MAke the image in knowyou page responsive

--- src/components/KnowYou.jsx
+++ src/components/KnowYou.jsx
@@ -4,7 +4,9 @@ import image from "../assets/images/image.jpeg"
 export default function KnowYou() {
   return (
     <div>
-      <img src={image} alt="" />
+      <div className="flex justify-center items-center mt-2 sm:justify-start">
+      <img src={image} alt="image"  width="220px" className='sm:w-[480px] object-cover'/>
+      </div>
     </div>
   )
 }
