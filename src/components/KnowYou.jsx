3678e598c685bf87b14fa1d38d4a5ee7d19f4e4d
mbuimbogo <mbuimbogo99@gmail.com>
2023-04-12 00:52:27 +0300

    Add the home image

---
+++ src/components/KnowYou.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import image from "../assets/images/image.jpeg"
+
+export default function KnowYou() {
+  return (
+    <div>
+      <img src={image} alt="" />
+    </div>
+  )
+}
