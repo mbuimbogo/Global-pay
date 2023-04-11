66c905f922fb944abab3b8e5e9da8045da8b2ce9
mbuimbogo <mbuimbogo99@gmail.com>
2023-04-12 01:12:18 +0300

    Create home and about pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from './components/Header'
+import About from './pages/About'
+import Home from './pages/Home'
 
 function App() {
  
 
   return (
     <div >
-     <h1 className="text-red-400 underline">
-      Hello world!
-    </h1>
+      <Header/>
+     <Home/>
+     <About/>
     </div>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import KnowYou from '../components/KnowYou'
+
+export default function Home() {
+  return (
+    <div>
+      <KnowYou/>
+    </div>
+  )
+}
