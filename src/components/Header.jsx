0f4708af4ee9ef4cd70d6201f7cc761b7a650a8a
mbuimbogo <mbuimbogo99@gmail.com>
2023-04-12 00:52:00 +0300

    Create a responsive header and add icons for small screens

---
+++ src/components/Header.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="w-full bg-gray-200 top-0 z-10 flex justify-center align-center">
+      <div className="m-1 sm:hidden inline-flex space-x-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </div>
+
+      <ul className="inline-flex decoration-none m-1 space-x-3 hidden sm:inline-flex space-x-2">
+        <a href="/"> Home</a>
+        <a href="/about">About</a>
+      </ul>
+    </div>
+  );
+}
