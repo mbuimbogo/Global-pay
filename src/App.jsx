569d4929a93744b922d992b5543dc8f04d40a3e9
mbuimbogo <mbuimbogo99@gmail.com>
2023-04-12 01:29:19 +0300

    Add routing to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import Header from './components/Header'
-import About from './pages/About'
-import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 function App() {
- 
-
   return (
-    <div >
-      <Header/>
-     <Home/>
-     <About/>
+    <div>
+      <Header />
+      <Router>
+        <Routes>
+          <Route exact path="/" Component={Home} />
+          <Route path="/about" Component={About} />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
